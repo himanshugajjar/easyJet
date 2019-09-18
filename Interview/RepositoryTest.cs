0ed2b1890e0a4dd83a14d75ac2b3deda21d664c2
Himanshu Gajjar <gajjar.himanshu@gmail.com>
2019-09-18 22:57:39 +0100

    Get Method - red

--- Interview/RepositoryTest.cs
+++ Interview/RepositoryTest.cs
@@ -30,5 +30,22 @@ namespace Interview
             Assert.IsTrue(flight.Number == "U2 2265");
         }
 
+        [TestMethod]
+        public void Repository_get_should_retrun_valid_record()
+        {
+            //Arrange
+            var data = new List<Flight>() { new Flight { Id = 2, Number = "2Rendom" } };
+            var repo = new Repository(data);
+
+            //Action
+            var flight = repo.Get(2);
+
+            //Assert
+            Assert.IsNotNull(flight);
+
+            Assert.IsTrue(flight.Id == 2);
+
+            Assert.IsTrue(flight.Number == "2Random");
+        }
     }
 }
