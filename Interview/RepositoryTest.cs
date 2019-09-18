9396640a83c6c17b00c1f78577be1ef05672430f
Himanshu Gajjar <gajjar.himanshu@gmail.com>
2019-09-18 22:10:20 +0100

    Save record Test - red

---
+++ Interview/RepositoryTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Interview
+{
+    [TestClass]
+    public class RepositoryTest
+    {
+        [TestMethod]
+        public void Repository_save_should_add_record_to_in_memory()
+        {
+            //Arrange
+            var repo = new Repository();
+
+            //Action
+            repo.Save(new Flight()
+            {
+                Id = 1,
+                Number = "U2 2265"
+            });
+
+            var flight = repo.Get(1);
+
+
+            //Assert
+            Assert.IsNotNull(flight);
+
+            Assert.IsTrue(flight.Id == 1);
+
+            Assert.IsTrue(flight.Name == "U2 2265");
+        }
+    }
+}
